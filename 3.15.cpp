fee496ae889f8ab2eef38fab5f7658add25dff06
chicadeamarilla <rajabisheyda15@gmail.com>
2024-12-29 19:52:39 +0330

    Create 3.15.cpp
    Create my first git

---
+++ 3.15.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include<string>
+using namespace std;
+class date {
+
+private:
+	int month;
+	int day;
+	int year;
+
+public:
+ date (int m,int d,int y) {
+		month=m;
+		day=d;
+		year=y;
+	}
+	
+
+	void setmonth(int m){
+	    if(m>=1&&m<=12) {
+			month=m;
+		} else {
+			month=1;
+		}
+	}
+	
+	void setday(int d){
+	    day=d;
+	}
+	
+	void setyear(int y){
+	    year=y;
+	}
+	
+
+	int getmonth(){
+	    return month;
+	}
+	int getday(){
+	    return day;
+	}
+	int getyear(){
+	    return year;
+	}
+	void displaydate(){
+	    cout<<month<<"/"<<day<<"/"<<year<<endl;
+	}
+};
+
+int main()
+{
+    date date1(10,20,1996);
+    
+    cout<<"initial date:";
+    date1.displaydate();
+    
+    date1.setmonth(1);
+    date1.setday(1);
+    date1.setyear(2020);
+    
+    cout<<"updated date:";
+    date1.displaydate();
+    
+	return 0;
+}
